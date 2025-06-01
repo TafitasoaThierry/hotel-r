cef595b48df8175c22676db6e6c5f82e24d0705c
TafitasoaThierry <nattafitasoa18@gmail.com>
2025-06-01 18:32:34 +0300

    dashboard

--- src/pages/admin/dashboard.tsx
+++ src/pages/admin/dashboard.tsx
@@ -1,17 +1,18 @@
 import { useEffect, useState } from "react";
 import "../../styles/admin.dashboard.scss";
 import ReservationObj from "../../fetch/reservation";
+import { BarChart } from "@mui/x-charts";
 
 export default function Dashboard() {
     return (
         <div className="dashboard-container">
-            <div><Reservation /></div>
+            <Reservation />
         </div>
     );
 }
 
 function Reservation() {
-    const [recent, setRecent] = useState(12);
+    const [recent, setRecent] = useState(0);
     const [total, setTotal] = useState(0);
     const [ceMois, setCeMois] = useState(0);
 
@@ -41,33 +42,27 @@ function Reservation() {
 
     return (
         <div className="reservation-container">
-            <div className="box">
-                <div className="box-item">
-                    <h5>Nouvelle réservation</h5>
-                    <h1 className="value"><i className="fa-solid fa-history"></i> {recent}</h1>
+            <div className="data">
+                <div className="box-list">
+                    <div className="box nouvelle">
+                        <h5>Nouvelle réservation</h5>
+                        <h3>{ recent }</h3>
+                    </div>
+                    <div className="box totale">
+                        <h5>Totale réservation</h5>
+                        <h3>{ total }</h3>
+                    </div>
+                    <div className="box ce-mois">
+                        <h5>Recette de ce mois</h5>
+                        <h3>{ ceMois }</h3>
+                    </div>
                 </div>
-                <div className="box-item">
-                    <h5>Total réservation</h5>
-                    <h1 className="value"><i className="fa-solid fa-total"></i> {total}</h1>
-                </div>
-                <div className="box-item">
-                    <h5>Recette de ce mois</h5>
-                    <h1 className="value"><i className="fa-solid fa-entry"></i> {ceMois}</h1>
-                </div>
+                <Histogramme />
             </div>
-            <h3>Listes de reservation</h3>
             <div className="search-field">
                 <input type="text" className="form-control" placeholder="Rechercher de réservation(nom, prenoms)" />
                 <button className="btn"><i className="fa-solid fa-search"></i> Rechercher</button>
             </div>
-            <div className="reservation-list">
-                <div className="list">Réservation de 1 chambre classique pour 2 personnes</div>
-                <div className="list">Réservation de 1 chambre classique pour 2 personnes</div>
-                <div className="list">Réservation de 1 chambre classique pour 2 personnes</div>
-                <div className="list">Réservation de 1 chambre classique pour 2 personnes</div>
-                <div className="list">Réservation de 1 chambre classique pour 2 personnes</div>
-            </div>
-            <div><Histogramme /></div>
             <div className="reservation-list">
                 <div className="list">Réservation de 1 chambre classique pour 2 personnes</div>
                 <div className="list">Réservation de 1 chambre classique pour 2 personnes</div>
@@ -81,6 +76,22 @@ function Reservation() {
 
 function Histogramme() {
     return (
-        <h1>Histogramme</h1>
+        <div className="histogramme">
+            <h2>Recette mensuelle</h2>
+            <BarChart
+                xAxis={[
+                    {
+                    id: 'barCategories',
+                    data: ['Jan', 'Fev', 'Mars', 'Avr', 'Mai', 'Jui','Juil', 'Août', 'Sep', 'Oct', 'Nov', 'Dec'],
+                    },
+                ]}
+                series={[
+                    {
+                    data: [2, 5, 3, 2, 5, 3, 2, 5, 3, 23, 12, 31],
+                    },
+                ]}
+                height={280}
+            />
+        </div>
     );
 }
