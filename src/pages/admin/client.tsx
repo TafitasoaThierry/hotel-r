3d6316992bc146f7ef069f9804ec50037ed7514d
TafitasoaThierry <nattafitasoa18@gmail.com>
2025-06-04 23:10:47 +0300

    display client list

--- src/pages/admin/client.tsx
+++ src/pages/admin/client.tsx
@@ -1,10 +1,46 @@
+import { useEffect, useState } from "react";
+import ClientObj from "../../fetch/client";
+
 export default function Client() {
+    const [clientList, setClientList] = useState([]);
+    
+    useEffect(() => {
+        const client = new ClientObj();
+        client.obtenirListeClient()
+        .then((response) => {
+            setClientList(response.data);
+            console.log(client);
+        })
+    }, [])
     return (
         <div className="client-container">
             <h1>Listes client</h1>
             <table className="table">
-                // 
+                <thead>
+                    <tr>
+                        <th>Numero client</th>
+                        <th>Nom</th>
+                        <th>Prenoms</th>
+                        <th>Tel</th>
+                        <th>Email</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {clientList.map((client:any) => <ListClient key={client.numeroClient} client={client} />)}
+                </tbody>
             </table>
         </div>
+    );
+}
+
+function ListClient({client}: any) {
+    return (
+        <tr>
+            <td>client.numeroCl</td>
+            <td>client.nom</td>
+            <td>client.prenoms</td>
+            <td>client.tel</td>
+            <td>client.email</td>
+        </tr>
     );
 }
